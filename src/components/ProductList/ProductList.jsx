c951acc529bd38bcbcded613875074d1fec0cc7c
SmatovE <black63squad@gmail.com>
2024-07-03 18:44:11 +0500

    products

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,14 +5,14 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Курс 1', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Курс', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Курс 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Курс 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Курс 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Курс 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Курс 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Курс 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
 const getTotalPrice = (items = []) => {
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://185.129.51.159:8000/web-data', {
+        fetch('http://localhost:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
