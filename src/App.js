5cfb227be4ec77ff4d6a9bcd810deee54e5ef3dd
SmatovE <black63squad@gmail.com>
2024-07-03 19:00:04 +0500

    products

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, {useEffect} from 'react';
-import {useTelegram} from './hooks/useTelegram';
-import Header from './components/Header/Header';
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
-import {Route, Routes} from 'react-router-dom';
 import './App.css';
-function App() {
-  const {tg} = useTelegram();
-  
-  useEffect(() => {
-    tg.ready();
-  }, []);
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route index element={<ProductList />} />
-        <Route path={'form'} element={<Form />} />
-      </Routes>
-    </div>
-  );
+function App() {
+    const {tg} = useTelegram();
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,22 +1,23 @@
 const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
+
     const onClose = () => {
-        tg.close();
-    };
+        tg.close()
+    }
 
     const onToggleButton = () => {
-        if (tg.MainButton.isVisible) {
+        if(tg.MainButton.isVisible) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
         }
-    };
+    }
 
     return {
-        tg,
         onClose,
         onToggleButton,
+        tg,
         user: tg.initDataUnsafe?.user,
         queryId: tg.initDataUnsafe?.query_id,
     }
