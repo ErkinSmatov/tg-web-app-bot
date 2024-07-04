28fbe0e6e2827b8abe929409330d253abf668b9c
SmatovE <black63squad@gmail.com>
2024-07-04 17:58:30 +0500

    update

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -33,7 +33,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://185.129.51.159:8000/web-data', {
+        fetch('http://185.129.51.159:8898/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
